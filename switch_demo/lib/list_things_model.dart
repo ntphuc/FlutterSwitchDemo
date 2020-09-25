5cc0528c72e22902d633a19faa4483f154f3e511
Phuc Nguyen <thienphuc0510@gmail.com>
2020-09-25 20:54:27 +0700

    parse json with filter

--- switch_demo/lib/list_things_model.dart
+++ switch_demo/lib/list_things_model.dart
@@ -8,7 +8,7 @@ class ListThingsModel {
   factory ListThingsModel.fromJson(Map<String, dynamic> json) {
     var list = json['rows'] as List;
     List<CellModel> listThings = list.map((e) => CellModel.fromJson(e)).toList();
-    //listThings  = listThings.where((item) => item.name.contains("Switch_")).toList();
+    listThings  = listThings.where((item) => item.name.contains("Switch_")).toList();
 
     return ListThingsModel(
         rows: listThings
