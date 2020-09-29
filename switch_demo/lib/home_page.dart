8e380484b5f3b6be07a7bac05861b361334d56e4
Phuc Nguyen <thienphuc0510@gmail.com>
2020-09-29 17:11:28 +0700

    add firebase notification

--- switch_demo/lib/home_page.dart
+++ switch_demo/lib/home_page.dart
@@ -1,11 +1,67 @@
 import 'dart:async';
 
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'cell_model.dart';
 import 'common_comp.dart';
 import 'constants.dart';
 import 'services.dart';
 import 'switches_model.dart';
+
+Future<dynamic> myBackgroundMessageHandler(Map<String, dynamic> message) {
+  if (message.containsKey('data')) {
+    final dynamic data = message['data'];
+  }
+
+  if (message.containsKey('notification')) {
+    final dynamic notification = message['notification'];
+  }
+
+}
+
+final Map<String, Item> _items = <String, Item>{};
+Item _itemForMessage(Map<String, dynamic> message) {
+  final dynamic data = message['data'] ?? message;
+  final String itemId = data['id'];
+  final Item item = _items.putIfAbsent(itemId, () => Item(itemId: itemId))
+    .._matchteam = data['name']
+    .._score = data['status'];
+  return item;
+}
+
+class Item {
+  Item({this.itemId});
+  final String itemId;
+
+  StreamController<Item> _controller = StreamController<Item>.broadcast();
+  Stream<Item> get onChanged => _controller.stream;
+
+  String _matchteam;
+  String get matchteam => _matchteam;
+  set matchteam(String value) {
+    _matchteam = value;
+    _controller.add(this);
+  }
+
+  String _score;
+  String get score => _score;
+  set score(String value) {
+    _score = value;
+    _controller.add(this);
+  }
+
+//  static final Map<String, Route<void>> routes = <String, Route<void>>{};
+//  Route<void> get route {
+//    final String routeName = '/detail/$itemId';
+//    return routes.putIfAbsent(
+//      routeName,
+//          () => MaterialPageRoute<void>(
+//        settings: RouteSettings(name: routeName),
+//        builder: (BuildContext context) => DetailPage(itemId),
+//      ),
+//    );
+//  }
+}
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key}) : super(key: key);
@@ -21,9 +77,13 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Duration TIME_DELAY_UPDATE = Duration(seconds: 5);
 
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
+
   @override
   void initState() {
     super.initState();
+    registerPushNotification();
     isHomeDataLoading = true;
     runTimeDelay();
   }
@@ -90,6 +150,69 @@ class _MyHomePageState extends State<MyHomePage> {
     timer?.cancel();
     super.dispose();
   }
+
+  void registerPushNotification() {
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+        _showItemDialog(message);
+      },
+      onBackgroundMessage: myBackgroundMessageHandler,
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+        //_navigateToItemDetail(message);
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+        //_navigateToItemDetail(message);
+      },
+    );
+    _firebaseMessaging.requestNotificationPermissions(
+        const IosNotificationSettings(
+            sound: true, badge: true, alert: true, provisional: true));
+    _firebaseMessaging.onIosSettingsRegistered
+        .listen((IosNotificationSettings settings) {
+      print("Settings registered: $settings");
+    });
+    _firebaseMessaging.getToken().then((String token) {
+      assert(token != null);
+      print("Push Messaging token: $token");
+    });
+    _firebaseMessaging.subscribeToTopic("matchscore");
+  }
+
+
+  Widget _buildDialog(BuildContext context, Item item) {
+    return AlertDialog(
+      content: Text("${item.matchteam} with score: ${item.score}"),
+      actions: <Widget>[
+        FlatButton(
+          child: const Text('CLOSE'),
+          onPressed: () {
+            Navigator.pop(context, false);
+          },
+        ),
+        FlatButton(
+          child: const Text('SHOW'),
+          onPressed: () {
+            Navigator.pop(context, true);
+          },
+        ),
+      ],
+    );
+  }
+
+  void _showItemDialog(Map<String, dynamic> message) {
+    showDialog<bool>(
+      context: context,
+      builder: (_) => _buildDialog(context, _itemForMessage(message)),
+    ).then((bool shouldNavigate) {
+      if (shouldNavigate == true) {
+        //_navigateToItemDetail(message);
+      }
+    });
+  }
+
 }
 
 gridClicked(BuildContext context, CellModel cellModel) {
